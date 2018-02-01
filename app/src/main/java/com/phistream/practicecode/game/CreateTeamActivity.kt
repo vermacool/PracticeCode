80ff03f0850cf282aae063c4546fef509b704837
BiswajitRout <Biswajitrout456@gmail.com>
2018-02-01 18:48:03 +0530

    Create Team activity added

--- app/src/main/java/com/phistream/practicecode/game/CreateTeamActivity.kt
+++ app/src/main/java/com/phistream/practicecode/game/CreateTeamActivity.kt
@@ -4,20 +4,17 @@ import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import com.phistream.practicecode.R
+import io.realm.Realm
 
 import kotlinx.android.synthetic.main.activity_create_team.*
 
 class CreateTeamActivity : AppCompatActivity() {
-
+    lateinit var  realm: Realm
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_team)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
---
+++ app/src/main/java/com/phistream/practicecode/game/TeamPagerAdapter.kt
@@ -0,0 +1,19 @@
+package com.phistream.practicecode.game
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+
+/**
+ * Created by Biswajit on 2/1/2018.
+ * All rights reserved by BiswajitApps
+ */
+class TeamPagerAdapter(var fragmentManager: FragmentManager): FragmentStatePagerAdapter(fragmentManager) {
+    override fun getItem(position: Int): Fragment {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
